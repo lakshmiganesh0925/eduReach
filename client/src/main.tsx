95e122ddce60058e74e7beaeb44a6f4707e1e367
lakshmiganesh0925 <lakshmiganesh0925@gmail.com>
2026-03-19 14:55:29 +0530

    change

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
